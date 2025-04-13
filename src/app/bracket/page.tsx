b154f6df07e0e21228a57f31c4ae1d3deef06cf4
k-redstone <che0618@naver.com>
2025-04-14 00:06:57 +0900

    chore: 결승전 1세트 결과반영

--- src/app/bracket/page.tsx
+++ src/app/bracket/page.tsx
@@ -161,7 +161,7 @@ export default function BracketPage() {
                     <div className="flex items-center gap-1">
                       <span className="text-xs">노코멘트</span>
                     </div>
-                    <div className="font-bold">0</div>
+                    <div className="font-bold">1</div>
                   </div>
                 </div>
               </CardContent>
